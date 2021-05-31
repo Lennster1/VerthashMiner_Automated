6430426d7051ff81d7018db8b81ee4bfe55f869f
Lennster1 <lennyluo0709@gmail.com>
2021-05-31 09:48:11 -0400

    Update cmonwork.bat

--- Assets/cmonwork.bat
+++ Assets/cmonwork.bat
@@ -13,10 +13,11 @@ If %Verthashdat%==False (
     echo Verthash.dat does not Exist! 
     start generate_vtc_dat.bat
 )
-If %batExists%==True (
+If %batExists%==False (
+    set /P walletAdress="What is your wallet adress?:"
+    echo (VerthashMiner -u %walletAdress% -p x -o stratum+tcp://verthash.mine.zergpool.com:4534 --verthash-data verthash.dat --all-cl-devices --all-cu-devices) >work.bat
     start work.bat
 ) else (
-    set /P walletAdress=What is your wallet adress?:
-    (echo
-    VerthashMiner -u %walletAdress% -p x -o stratum+tcp://verthash.mine.zergpool.com:4534 --verthash-data verthash.dat --all-cl-devices --all-cu-devices) >work.bat
     start work.bat
+)
+pause
